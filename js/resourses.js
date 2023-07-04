0e0f31aafbadc83a788a9533a02cfdb5d73f1c68
Роман <roman-li-83@yandex.ru>
2023-07-04 23:06:58 +0500

    Continue separate to classes in new branch "develop"

---
+++ js/resourses.js
@@ -0,0 +1,11 @@
+export let arrColors = [
+	'#1ae81a',
+	'#d0aa15',
+	'#ea873f',
+	'#3067f8',
+	'#ab11ce',
+	'#e83939'];
+	
+let arrAnimate = ['animate-rotate-turn','animate-rotate-deg','animate-scale'];
+let arrFigures = ['oval','square','circle'];
+export let classesByItem = [arrAnimate, arrFigures];
