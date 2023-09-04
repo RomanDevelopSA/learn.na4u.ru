2a9f51eb64029646456dcc7edd423d65113be9f3
Роман <roman-li-83@yandex.ru>
2023-09-04 22:48:04 +0500

    Start develop work calculate

--- js/Alphabet.js
+++ js/Alphabet.js
@@ -5,61 +5,29 @@ import * as res from './resourses.js';
 export default class Alphabet{
     
     constructor(){
-		this.blockLetters = document.querySelector(".block-letters");
-		this.blockNumbers = document.querySelector(".block-numbers");
+		this.taskContent = document.querySelector(".task_content");
         this.lessons = new Lessons();
         this.classes = this.lessons.classesToAdd(res.classesByItem);
-		//this.genDataShowName(res.arrData);
-		this.createStartItems(res.startArrData);
     }
 
-	createStartItems(arrData,container=null){
-		for (let number of res.arrNumbers){
-			let div = document.createElement('div');
-			div.dataset.show = "number";
-			div.classList.add("common");
-			div.innerHTML = number;
-			this.blockNumbers.append(div);
-
-		}
-
+	createStartItems(container=null){
 		for (let letter of res.arrLetters){
-
 			let div = document.createElement('div');
-			div.dataset.show = "letter";
+			div.dataset.key = "letter";
 			div.classList.add("common");
 			div.innerHTML = letter;
-			this.blockLetters.append(div);
+			this.taskContent.append(div);
 
 		}
 	}
 
-	createShowItem(result, event){
+	createShowItem(result, content){
 
 	    let div = document.createElement('div');
 	    div.classList.add('letter',...this.classes);
-	    div.innerHTML = event.target.innerHTML;
+	    div.innerHTML = content;
 	    div.style.backgroundColor = this.lessons.getRandomColor(res.arrColors);
 	    result.append(div);
 	}
-
-
-	genDataShowName(arrData){
-
-		arrData.forEach((item)=>
-		{
-			console.log("item: "+item);
-		})
-		for (let item of arrData){
-			console.log("item: "+item);
-			/*if (item.startsWith("arr")){
-				console.log("item length: "+item.length);
-			}else {
-				alert("Неверное имя метода");
-			}*/
-		}
-
-	}
-
 	
 }
--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 import Alphabet from './Alphabet.js';
+import Numbers from "./Numbers.js";
 
 let main = document.querySelector("main");
+let taskContent = document.querySelector(".task_content");
+let calculator = document.querySelector(".calculator");
 
 let result = document.querySelector(".result");
 let reset = document.querySelector(".reset");
@@ -8,36 +11,70 @@ let res = null;
 
 
 let alphabet = new Alphabet();
+let numbers = new Numbers();
 
-document.addEventListener('click',testClick);
-function testClick(event){
+alphabet.createStartItems();
 
-    let elem1 = event.target.dataset.show;
-    if(res === null){
-        res = document.createElement('div');
-        res.classList.add('result');
+function createBlockResult(){
+    if (res === null) {
+       res = document.createElement('div');
+       res.classList.add('result');
         main.after(res);
-    }
-
-    switch(elem1){
-        case "number":
-            console.log("num: "+event.target.innerHTML);
-            alphabet.createShowItem(res,event);
-            break;
-        case "letter":
-            console.log("letters: "+event.target.innerHTML);
-            alphabet.createShowItem(res,event);
-            break;
     }
 }
 
 
-reset.addEventListener("click",()=> {
-    if(res !== null){
+taskContent.addEventListener('click', onTaskClick);
+calculator.addEventListener('click', onCalculatorClick);
+
+
+function onTaskClick(event) {
+
+    let key = event.target.dataset.key;
+    let keyContent = event.target.innerHTML;
+    
+    createBlockResult();
+
+    switch (key) {
+        case "letter":
+            console.log("letters: " + event.target.innerHTML);
+            alphabet.createShowItem(res, keyContent);
+            break;
+    }
+
+}
+
+function onCalculatorClick(event) {
+
+    let key = event.target.dataset.key;
+    let keyContent = event.target.innerHTML;
+    
+    createBlockResult();
+
+    switch (key) {
+        case "add":
+            console.log("add" );
+            numbers.createShowItem(res, keyContent);
+            break;
+        case "subtract":
+            console.log("subtract");
+            break;
+        case "calculate":
+            console.log("calculate");
+            break;
+        default:
+            console.log("reset");
+            break;
+
+    }
+
+}
+
+
+reset.addEventListener("click", () => {
+    if (res !== null) {
         res.remove();
         res = null;
     }
-
-    alphabet = new Alphabet();
 
 })
